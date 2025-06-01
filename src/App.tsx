58316910c61fc37c6b8edd29925f137356c44b0d
jquinteromo <qjaider1111@gmail.com>
2025-06-01 15:43:23 -0500

    Init Dev Estructure moodflix

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[#FDF2F8] text-[#111827] flex items-center justify-center">
+      <h1 className="text-4xl bg font-bold">Bienvenido a MoodFlix 🎬</h1>
+    </div>
+  );
+}
+
+export default App;
