392933951ef8fac5924192a634ba0e001a70a7f1
jquinteromo <qjaider1111@gmail.com>
2025-06-02 18:50:53 -0500

    Optimitation loading img Banner

--- src/Pages/Home/Banner.tsx
+++ src/Pages/Home/Banner.tsx
@@ -1,5 +1,4 @@
-import {  useEffect, useState } from "react";
-import LazyLoad from "vanilla-lazyload"; 
+import { useEffect, useState } from "react";
 
 type MovieType = {
   id: number;
@@ -15,50 +14,57 @@ const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export default function Banner() {
-  const [movies, setMovies] = useState<MovieType[]>([]);
+//   const [movies, setMovies] = useState<MovieType[]>([]);
+  const [randomMovie, setRandomMovie] = useState<MovieType | null>(null);
+  const [src, setSrc] = useState("");
+
   useEffect(() => {
-    fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=es-ES&page=3`)
+    fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=es-ES&page=5`)
       .then((res) => res.json())
-      .then((data) => setMovies(data.results));
+      .then((data) => {
+        // setMovies(data.results);
+
+        const filteredMovies = data.results.filter((m:MovieType) => m.backdrop_path);
+        const random =
+          filteredMovies[Math.floor(Math.random() * filteredMovies.length)];
+        setRandomMovie(random);
+      });
   }, []);
 
-useEffect(() => {
-    if (movies.length > 0) {
-      new LazyLoad({
-        elements_selector: ".lazyload",
-        threshold: 300, 
-      });
+  useEffect(() => {
+    if (!randomMovie) return;
 
-    }
-  }, [movies]);
+    const low = `https://image.tmdb.org/t/p/w300${randomMovie.backdrop_path}`;
+    const highRes = `https://image.tmdb.org/t/p/original${randomMovie.backdrop_path}`;
+    setSrc(low);
 
-  const filteredMovies = movies.filter(m => m.backdrop_path);
-  const randomMovies = [...filteredMovies]  
-  .sort(() => Math.random() - 0.5)
-  .slice(0, 1); 
+    const img = new Image();
+    img.src = highRes;
+    img.onload = () => setSrc(highRes);
+  }, [randomMovie]);
 
-
+  if (!randomMovie) return null;
   return (
     <div>
-      {randomMovies.map((movie) => (
-        <div key={movie.id} className="relative w-full h-[70vh] bg-white aspect-video overflow-hidden">
-          <img
-            src={`https://image.tmdb.org/t/p/w92${movie.backdrop_path}`}
-             data-src={`https://image.tmdb.org/t/p/w780${movie.backdrop_path}`}
-            data-srcset={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-            alt="Banner"
-            className="lazyload w-full h-full object-cover transition-opacity duration-700"
-          ></img>
-          <div className="absolute inset-0 bg-gradient-to-r from-black via-transparent to- 
-            transparent"></div>
-          <div className="absolute inset-0 flex  justify-center flex-col px-8">
-            <div className="text-white max-w-xl">
-              <h1 className="text-4xl font-bold">{movie.title}</h1>
-              <p className="mt-2 text-lg">{movie.overview.slice(0, 100)}</p>
-            </div>
+      <div className="relative w-full h-[70vh] bg-white aspect-video overflow-hidden">
+        <img
+          // src={`https://image.tmdb.org/t/p/w300${movie.backdrop_path}`}
+          src={src}
+          sizes="100vw"
+          alt="Banner"
+          className="w-full h-full object-cover transition-opacity duration-700"
+        ></img>
+        <div
+          className="absolute inset-0 bg-gradient-to-r from-black via-transparent to- 
+            transparent"
+        ></div>
+        <div className="absolute inset-0 flex  justify-center flex-col px-8">
+          <div className="text-white max-w-xl">
+            <h1 className="text-4xl font-bold">{randomMovie.title}</h1>
+            <p className="mt-2 text-lg">{randomMovie.overview.slice(0, 200)}...</p>
           </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
